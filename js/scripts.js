4d1ca29442ee58fc1cba410187fd659c3450e26d
Chris DePastene <cdepastene@gmail.com>
2021-11-07 20:41:25 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  const favsArray = [];
+  
+  $("#favorite-things").submit(function() {
+    event.preventDefault();
+
+    favsArray.push($("#pizza1").val());
+    favsArray.push($("#pizza2").val());
+    favsArray.push($("#pizza3").val());
+    favsArray.push($("#bands1").val());
+    favsArray.push($("#bands2").val());
+    favsArray.push($("#bands3").val());
+    favsArray.push($("#animals1").val());
+    favsArray.push($("#animals2").val());
+    favsArray.push($("#animals3").val());
+    
+    $("#fav1").text(favsArray[0]);
+    $("#fav2").text(favsArray[5]);
+    $("#fav3").text(favsArray[7]);
+  });
+  
+});
